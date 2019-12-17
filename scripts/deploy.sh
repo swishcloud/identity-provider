85f83a83b50d8ce24e9768a9077000ed86bafd5c
swishcloud <flwwd@outlook.com>
2019-12-17 23:17:20 +0800

    cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker push $IMAGE_TAG
-sshpass -p $SSH_DEPLOY_PASSWORD $SSHCOMMAND $TRAVIS_COMMIT
+sshpass -p $SSH_DEPLOY_PASSWORD $SSH_CMMAND $TRAVIS_COMMIT
 if [ $? -ne 0 ]; then
     echo "deploy failed"
     exit 1
