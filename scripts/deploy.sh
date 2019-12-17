eb8757d9805eded2407003e8cd5343366a17f259
allen <allen@DESKTOP-BM3TNO6>
2019-12-17 17:51:09 +0800

    cd

---
+++ scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+docker push $IMAGE_TAG
+sshpass -p $SSH_DEPLOY_PASSWORD $SSHCOMMAND $TRAVIS_COMMIT
+if [ $? -ne 0 ]; then
+    echo "deploy failed"
+    exit 1
+else
+    echo "-----------------------------"
+    echo "deploy completed successfully"
+    echo "-----------------------------"
+fi
