c84dce290e2a183a1b1ebca6feffb830fa6f35ed
allen <allen@DESKTOP-BM3TNO6>
2019-12-19 17:02:48 +0800

    add migrations to docker image

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,5 @@
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./.dist/app
 cp templates  ./.dist -r
 cp static  ./.dist -r
+cp migrations  ./.dist -r
 docker build --tag $IMAGE_TAG -f docker/dockerfile ./.dist
