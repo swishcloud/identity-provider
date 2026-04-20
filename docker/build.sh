e773860038a012ba6286431c7a0439cdc281991b
Your Name <you@example.com>
2026-04-20 15:28:36 +0800

    update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,22 +1,6 @@
 #/bin/sh
-if [ ! -d .cache ];then
-mkdir .cache
-fi
-if [ -z $REPO ]
-then
-REPO='idp'
-fi
-TAG=`git log| grep '(?<=^commit ).+' -m1 -oP`
-echo 'image tag:'$TAG
-
-#some variables
-export PROGRAM_NAME='idp'
-
-#build the docker image
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./.dist/$PROGRAM_NAME
-#copy files need for the image to the dist folder
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./.dist/idp
 cp templates  ./.dist -r
 cp static  ./.dist -r
 cp migrations  ./.dist -r
-#buiding
-sudo docker build --tag $REPO:$TAG --tag $REPO -f docker/dockerfile ./.dist
+sudo docker build --tag $IMAGE_TAG -f docker/dockerfile ./.dist
